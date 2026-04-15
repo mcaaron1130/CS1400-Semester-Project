deffc58dd44f5a62d8cd9554dbf2bf1c4cff7d74
mcaaron1130 <mcaaron1130@gmail.com>
2026-04-14 20:10:38 -0700

    Implement main method to handle book title input
    Added functionality to read and display book title.

--- Store.java
+++ Store.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class MediaStore {
-  public static void main (String[] args) {
-    
-  }
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Book book1 = new Book();
+        String title;
+
+        title = scnr.nextLine();
+        book1.setTitle(title);
+        System.out.println(book1.getTitle());
+
+    }
 }
