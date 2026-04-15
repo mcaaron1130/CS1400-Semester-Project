ed62394609987fe37b0f23c1275cb15665913baa
mcaaron1130 <mcaaron1130@gmail.com>
2026-04-14 20:03:30 -0700

    Add Book class with title property and methods

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    
+    private String title;
+    private String genre;
+
+    public void setTitle(String inputTitle) {
+       title = inputTitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
