114af1068f3fc1eee1f342318c4f24b241ae550d
mcaaron1130 <mcaaron1130@gmail.com>
2026-04-22 22:19:58 -0700

    Convert Book class to Movie class with new attributes
    Refactored Book class to Movie class with title and company attributes.

---
+++ Movie.java
@@ -0,0 +1,19 @@
+public class Movie {
+    
+    private String title;
+    private String company;
+
+    public Movie(String title, String company) {
+        this.title = title;
+        this.company = company;
+    } 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+}
+
