17fbb6830a2c20ffe876a0ea5ac4ddefcc3a1324
mcaaron1130 <mcaaron1130@gmail.com>
2026-05-01 21:56:55 -0700

    Refactor Movie class to include year and price
    Updated Movie class to include year and price attributes, removed company attribute, and modified constructor and methods accordingly.

--- Movie.java
+++ Movie.java
@@ -1,19 +1,33 @@
 public class Movie {
     
     private String title;
-    private String company;
+    private int year;
+    private double price;  
 
-    public Movie(String title, String company) {
+    public Movie(String title, int year) {
         this.title = title;
-        this.company = company;
-    } 
+        this.year = year;
+        this.price = 0.0;
+    }
 
     public String getTitle() {
         return title;
     }
 
-    public String getCompany() {
-        return company;
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        if(year < 2010) {
+            price = 15.99;
+        }
+        else if (year >= 2010 && year < 2020) {
+            price = 19.99;
+        }
+        else {
+            price = 25.99;
+        }
+        return price;
     }
 }
-
