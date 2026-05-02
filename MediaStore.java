595cc636916e10c4d482c69a98b26750c24ffb40
mcaaron1130 <mcaaron1130@gmail.com>
2026-05-01 22:03:16 -0700

    Enhance MediaStore to support books shopping
    Updated MediaSystem initialization and added shopping options for both movies and books.

--- MediaStore.java
+++ MediaStore.java
@@ -5,10 +5,28 @@ public class MediaStore {
     public static void main(String[] args) throws IOException {
         Scanner scnr = new Scanner(System.in);
 
-        MediaSystem catalog = new MediaSystem ("src/movies.txt", "src/company.txt");
+        System.out.println("Welcome to the Media Store! You may choose up to 10 movies or books if you want.");
 
-        catalog.setMovieList();
-        
+        MediaSystem catalog = new MediaSystem("src/movies.txt", "src/movieYear.txt", "src/books.txt", "src/bookYear.txt");
+
+        System.out.println("Enter 1 to shop for movies or 2 to shop for books: ");
+        int choice = scnr.nextInt();
+
+        if(choice == 1) {
+            catalog.movieShoppingCart(scnr);
+            System.out.println("Would you like to shop for books too? Enter 1 for yes or 2 for no: ");
+            int choice2 = scnr.nextInt();
+            if(choice2 == 1) {
+                catalog.bookShoppingCart(scnr);
+            }
+        } else {
+            catalog.bookShoppingCart(scnr);
+            System.out.println("Would you like to shop for movies too? Enter 1 for yes or 2 for no: ");
+            int choice3 = scnr.nextInt();
+            if(choice3 == 1) {
+                catalog.movieShoppingCart(scnr);
+            }
+        }
         scnr.close();
     }
 }
